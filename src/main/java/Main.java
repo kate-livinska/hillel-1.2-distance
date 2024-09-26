2452c30a6af26000960a6c4590d781dfabad5fe8
Kateryna Livinska <kateryna.livinska@gmail.com>
2024-09-26 10:28:55 +0200

    Add app description

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
 public class Main {
 
+    public static void main(String[] args) {
+        System.out.println("Distance Converter App.");
+        System.out.println("App for converting distance units.");
+        System.out.println("Version 1.0");
+    }
 }
